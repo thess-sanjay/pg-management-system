1ccfdad4a9ef331a127bbecb310bf55523aa5af7
thess-sanjay <ss.sanjay3884@gmail.com>
2026-08-30 15:59:51 +0530

    Add vercel URL to CORS config

--- BackEnd/pg-management-system/src/main/java/com/pgms/pgmanagementsystem/config/SecurityConfig.java
+++ BackEnd/pg-management-system/src/main/java/com/pgms/pgmanagementsystem/config/SecurityConfig.java
@@ -52,8 +52,11 @@ public class SecurityConfig {
                 new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
-        );
+        	    List.of(
+        	        "http://localhost:5173",
+        	        "https://mypgmanager.vercel.app"
+        	    )
+        	);
 
         configuration.setAllowedMethods(
                 List.of(
